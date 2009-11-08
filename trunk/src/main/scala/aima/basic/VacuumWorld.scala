220123f4e025af8277ba8e11a52149af92ccfbf9
g.himanshu <g.himanshu@91973ec2-87f0-11de-a4f2-b7219ab9ea91>
2009-11-08 14:06:55 +0000

    rewritten agents code

---
+++ trunk/src/main/scala/aima/basic/VacuumWorld.scala
@@ -0,0 +1,132 @@
+package aima.basic
+
+object VacuumWorld {
+
+  type Percept = (String,String) //("A"/"B" , "Clean"/"Dirty")
+  type Action = String           // "Suck" / "Left" / "Right"
+  type State = Map[String,String]
+
+  //===================== Trivial Vacuum World Environment ====================
+  class TrivialVacuumEnvironment[T <: VacuumWorldAgent](private var statusA:String,
+                                                        private var statusB:String) 
+  extends Environment[T , Percept, Action] {
+    
+    import scala.util.Random
+
+    def this() = {
+      this( if(new Random().nextBoolean) "Clean" else "Dirty",
+           if(new Random().nextBoolean) "Clean" else "Dirty")
+    }
+    
+    override def executeAction(agent: T, action: Option[Action]) {
+      action match {
+        case None => agent.die()
+        case Some("Right") =>
+          agent location_= "B"
+        agent performance_= agent.performance - 1
+        case Some("Left") =>
+          agent location_= "A"
+        agent performance_= agent.performance - 1
+        case Some("Suck") =>
+          setStatusClean(agent.location)
+        agent performance_= agent.performance + 10
+        case Some(x) => throw new IllegalArgumentException("Invalid action. " + x)
+      }
+    }
+
+    override def getPerceptSeenBy(agent: T) =
+      agent.location match {
+        case "A" => ("A",statusA)
+          case "B" => ("B",statusB)
+            case location => throw new IllegalArgumentException("Unknown location. " + location)
+      }
+    
+    private def setStatusClean(location: String) {
+      location match {
+        case "A" => statusA = "Clean"
+        case "B" => statusB = "Clean"
+        case _ => throw new IllegalArgumentException("Unknown location. " + location)
+      }
+    }
+  }
+
+  //=============================== Agents ===================================
+
+  abstract class VacuumWorldAgent(var location: String, var performance: Int)
+           extends Agent[Percept,Action]
+
+  class TableDrivenVacuumAgent(l:String)
+  extends VacuumWorldAgent(l,0) with TableDrivenAgent[Percept,Action] {
+    override protected val table = {
+      var tmpTable:Map[List[Percept],Action] = Map();
+
+      tmpTable += List(("A","Clean")) -> "Right"
+      tmpTable
+    }
+  }
+
+  class ReflexVacuumAgent(l: String) extends VacuumWorldAgent(l,0) {
+    override def agentProgram(percept: Percept) = 
+      percept match {
+        case (_,"Dirty") => Some("Suck")
+        case ("A",_) => Some("Right")
+        case ("B",_) => Some("Left")
+        case _ => throw new IllegalArgumentException("Invalid Percept. " + percept)
+      }
+  }
+
+  import simplerule._
+
+  class SimpleReflexVacuumAgent(l: String) 
+  extends VacuumWorldAgent(l,0) with SimpleReflexAgent[State,Percept,Action] {
+    
+    override protected val rules = {
+      
+      // returns List(Rule(EqualCondition,Action))
+      def ruleGen(key: String, value: String, action: Action): List[Rule[State,Option[Action]]] =
+        List(new Rule(
+              new EqualCondition( (s: State) => (s.contains(key) && s.get(key) == Some(value))),
+              Some(action)))
+
+      var tmpRules: List[Rule[State,Option[Action]]] = Nil
+      tmpRules ++= ruleGen("status","Dirty","Suck")
+      tmpRules ++= ruleGen("location","A","Right")
+      tmpRules ++= ruleGen("location","B","Left")
+      tmpRules
+    }
+
+    override protected def interpretInput(percept: Percept) =
+      Map("location" -> percept._1, "status" -> percept._2)
+  }
+
+  class ReflexVacuumAgentWithState(l: String)
+  extends VacuumWorldAgent(l,0) with ReflexAgentWithState[State,Percept,Action] {
+    
+    override protected var state: State = Map()
+
+    override protected val rules = {
+      
+      // returns EqualCondition
+      def eqCondGen(key: String, value: String) =
+        new EqualCondition( (s: State) => (s.contains(key) && s.get(key) == Some(value)))
+
+      //returns List(Rule(EqualCondition,Action))
+      def ruleGen(key: String, value: String, action: Action): List[Rule[State,Option[Action]]] =
+        List(new Rule(eqCondGen(key,value),Some(action)))
+
+      var tmpRules: List[Rule[State,Option[Action]]] = Nil
+      tmpRules ++= List(new Rule(new AndCondition
+                                           (eqCondGen("statusA","Clean"),
+                                            eqCondGen("statusB","Clean")),None))
+      tmpRules ++= ruleGen("status", "Dirty", "Suck")
+      tmpRules ++= ruleGen("location", "A", "Right")
+      tmpRules ++= ruleGen("location", "B", "Left")
+      tmpRules
+    }
+
+    override protected def updateState(state: State, action: Option[Action], percept: Percept) =
+      state + ("location" -> percept._1,
+               "status" -> percept._2,
+               "status" + percept._1 -> percept._2)
+  }
+}
