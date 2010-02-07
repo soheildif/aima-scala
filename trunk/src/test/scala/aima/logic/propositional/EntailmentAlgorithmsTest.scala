95518f37a11604514a390c5f85507ee255ac88e4
g.himanshu <g.himanshu@91973ec2-87f0-11de-a4f2-b7219ab9ea91>
2010-02-07 04:23:44 +0000

    PL-Resolution impl..

--- trunk/src/test/scala/aima/logic/propositional/EntailmentAlgorithmsTest.scala
+++ trunk/src/test/scala/aima/logic/propositional/EntailmentAlgorithmsTest.scala
@@ -10,28 +10,45 @@ import scala.collection.immutable.{Set,ListSet}
  */
 
 
+/** Simple KB described in Fig 7.4.3 */
+object KB {
+  private val PLP = PropositionalLogicParser
+
+  def apply: Conjunction = {
+    val R1 = PLP.parse("~P11")
+    val R2 = PLP.parse("B11 <=> (P12 \\/ P21)")
+    val R3 = PLP.parse("B21 <=> (P11 \\/ P22 \\/ P31)")
+    val R4 = PLP.parse("~B11")
+    val R5 =PLP.parse( "B21")
+
+    Sentence.createKB(R1,R2,R3,R4,R5)
+  }
+}
+    
 class TTEntailsTest extends Suite {
   
   private val PLP = PropositionalLogicParser
 
-  /* This test is based on the simple knowledge base described in
-   * section 7.4.3
-   * KB = R1 /\ R2 /\ R3 /\ R4 /\ R5
+  /**
    * Test whether KB |= alpha, where alpha is ~P12?
    * It should return true.
    */
   def testSec7_4_3() {
-    //create the KB
-    val R1 = "~P11"
-    val R2 = "(B11 <=> (P12 \\/ P21))"
-    val R3 = "(B21 <=> (P11 \\/ P22 \\/ P31))"
-    val R4 = "~B11"
-    val R5 = "B21"
-
-    val KB = PLP.parse(R1 + "/\\" + R2 + "/\\" + R3 + "/\\" + R4 + "/\\" + R5)
     val alpha = PLP.parse("~P12")
-
-    assert(TTEntails(KB,alpha))
+    assert(TTEntails(KB.apply,alpha))
   }
 }
-    
+
+class PLResolutionTest extends Suite {
+  
+  private val PLP = PropositionalLogicParser
+
+  /**
+   * Test whether KB |= alpha, where alpha is ~P12?
+   * It should return true.
+   */
+  def testSec7_4_3() {
+    val alpha = PLP.parse("~P12")
+    assert(PLResolution(KB.apply,alpha))
+  }
+}
