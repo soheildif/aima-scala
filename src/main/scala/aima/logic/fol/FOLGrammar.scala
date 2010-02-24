07dbccf3560e714f5cbcffcafa9c91bc7cccb7e5
g.himanshu <g.himanshu@91973ec2-87f0-11de-a4f2-b7219ab9ea91>
2010-02-24 16:50:59 +0000

    fol Parser impl first draft ...wip

--- src/main/scala/aima/logic/fol/FOLGrammar.scala
+++ src/main/scala/aima/logic/fol/FOLGrammar.scala
@@ -1,6 +1,68 @@
 package aima.logic.fol
 
+import scala.util.parsing.combinator._
+
 /** Grammar for FOL logic */
+
+//AtomicSentence := Predicate(Term, Term, ...) | Term = Term
+
+//4L stands for FOR-All
+//3E stands for THERE EXISTS
+
+//Term = Function(Term,Term,...)| Variable | Constant
+
+object FOLParser extends JavaTokenParsers {
+
+  def sentence: Parser[Sentence] = (
+    "4L"~variable~biconditional ^^
+    { case _~v~s => new UniversalQuantifier(Variable(v),s) } //Universal Quantifier
+    | "3E"~variable~","~biconditional ^^
+    { case _~v~s => new ExistentialQuantifier(Variable(v),s) } //Existential Quantifier
+    | biconditional
+  )
+  def biconditional: Parser[Sentence] = conditional~opt("<=>"~conditional) ^^
+                                      {case condition~None => condition
+                                       case condition~Some(_~conclusion) => new BiConditional(condition,conclusion) }
+  def conditional: Parser[Sentence] = disjunction~opt("=>"~disjunction) ^^
+                                      {case premise~None => premise
+                                       case premise~Some(_~conclusion) => new Conditional(premise,conclusion) }
+  def disjunction: Parser[Sentence] = conjunction~rep("\\/"~conjunction) ^^
+                                      {case c~Nil => c
+                                       case c~reps => new Disjunction(c :: reps.map(_ match { case _~cond => cond }):_*)}
+  def conjunction: Parser[Sentence] = negation~rep("/\\"~negation) ^^
+                                      {case d~Nil => d
+                                       case d~reps => new Conjunction(d :: reps.map(_ match { case _~disj => disj }):_*)}
+  
+
+  def negation: Parser[Sentence] = rep("~")~atomicSentence ^^
+                                    {case Nil~as => as
+                                     case list~as => list.foldLeft(as)((x,y)=> new Negation(x))}
+
+  def atomicSentence: Parser[Sentence] = (
+    term~"="~term ^^
+    {case lt~"="~rt => new Equal(lt,rt)} //Equal
+    | symbol~"("~repsep(term,",")~")" ^^
+    {case s~"("~Nil~")" => new Predicate(s)
+     case s~"("~ts~")" => new Predicate(s,ts:_*)} //Predicate
+    | "(" ~> sentence <~ ")"
+  )
+                                         
+
+  def term: Parser[Term] = (
+    symbol~"("~repsep(term,",")~")" ^^
+    {case s~"("~Nil~")" => new Function(s)
+     case s~"("~ts~")" => new Function(s,ts:_*)}  //Function
+    | symbol ^^ (Constant(_)) //Constant
+    | variable ^^ (Variable(_)) //Variable
+  )
+
+  def variable = """[a-z][a-z0-9A-Z]*""".r //symbol for Variable
+  def symbol = """[A-Z][a-z0-9A-Z]*""".r //symbol for Constant,Predicate,Function
+
+  def parse(in: String) = parseAll(sentence,in).get
+}
+
+
 
 //TODO:Sentence equality should work
 abstract class Sentence
