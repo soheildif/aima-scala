abd9de298acc904aa5cb554bdbd183b427c4d52a
g.himanshu <g.himanshu@91973ec2-87f0-11de-a4f2-b7219ab9ea91>
2010-03-03 13:44:24 +0000

    fol... wip

--- trunk/src/test/scala/aima/logic/fol/FOLInferencesTest.scala
+++ trunk/src/test/scala/aima/logic/fol/FOLInferencesTest.scala
@@ -47,3 +47,48 @@ class FOLFCAskTest extends Suite {
          )(FOLFCAsk(KBFactory.kingsKB,query))
   }
 }
+
+/** Tests for FOL-BC-ASK
+ *
+ * @author Himanshu Gupta
+ */
+class FOLBCAskTest extends Suite {
+
+  def testWeaponsKBCriminalWestSuccess() {
+    val query = FOLParser.parse("Criminal(West)").asInstanceOf[AtomicSentence]
+    expect(Set(Map()))(FOLBCAsk(KBFactory.weaponsKB,query))
+  }
+
+  def testWeaponsKBCriminalXReturnsWest() {
+    val query = FOLParser.parse("Criminal(x)").asInstanceOf[AtomicSentence]
+
+    expect(Set(Map(Variable("x") -> Constant("West"))))(FOLBCAsk(KBFactory.weaponsKB,query))
+  }
+
+  def testKingsKBRichardEvilFalse() {
+    val query = FOLParser.parse("Evil(Richard)").asInstanceOf[AtomicSentence]
+    expect(Set())(FOLBCAsk(KBFactory.kingsKB,query))
+  }
+
+  def testKingsKBJohnEvilSuccess() {
+    val query = FOLParser.parse("Evil(John)").asInstanceOf[AtomicSentence]
+    expect(Set(Map()))(FOLBCAsk(KBFactory.kingsKB,query))
+  }
+
+  def testKingsKBEvilXReturnsJohn() {
+    val query = FOLParser.parse("Evil(x)").asInstanceOf[AtomicSentence]
+    expect(
+      Set(
+        Map(Variable("x")->Constant("John")))
+         )(FOLBCAsk(KBFactory.kingsKB,query))
+  }
+
+  def testKingsKBKingXReturnsJohnAndRichard() {
+    val query = FOLParser.parse("King(x)").asInstanceOf[AtomicSentence]
+    expect(
+      Set(
+        Map(Variable("x")->Constant("John")),
+        Map(Variable("x")->Constant("Richard")))
+         )(FOLBCAsk(KBFactory.kingsKB,query))
+  }
+}
