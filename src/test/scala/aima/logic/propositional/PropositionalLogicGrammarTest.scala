a45d85b9d4fc5ad5f57adc2f0a2e501c2e3686da
g.himanshu <g.himanshu@91973ec2-87f0-11de-a4f2-b7219ab9ea91>
2010-03-07 17:57:46 +0000

    correcting syntax for propositional logic parser, & -> /\ and | -> \/ now

--- src/test/scala/aima/logic/propositional/PropositionalLogicGrammarTest.scala
+++ src/test/scala/aima/logic/propositional/PropositionalLogicGrammarTest.scala
@@ -11,25 +11,55 @@ class PropositionalLogicParserTest extends Suite {
 
   private val PLP = PropositionalLogicParser
 
+
+  //some proposition symbols
+  val A = PropositionSymbol("A")
+  val B = PropositionSymbol("B")
+  val C = PropositionSymbol("C")
+  val D = PropositionSymbol("D")
+  val S = PropositionSymbol("S")
+  val T = PropositionSymbol("T")
+
+  val norvig = PropositionSymbol("norvig")
+  val aima = PropositionSymbol("aima")
+  val lisp = PropositionSymbol("lisp")
+  val cool = PropositionSymbol("cool")
+
   def testParser() {
 
-    assert(PLP.parse("A").toString == "A")
-    assert(PLP.parse("AIMA").toString == "AIMA")
-    assert(PLP.parse("True").toString == "True")
-    assert(PLP.parse("False").toString == "False")
-    assert(PLP.parse("~ A12").toString == "~A12")
-    expect("(A \\/ B)")(PLP.parse("A\\/B").toString)
-    assert(PLP.parse("A /\\ B /\\ C").toString == "(A /\\ B /\\ C)")
-    assert(PLP.parse("A \\/ B \\/ C").toString == "(A \\/ B \\/ C)")
-    assert(PLP.parse("A \\/ B /\\ C").toString == "(A \\/ (B /\\ C))")
-    assert(PLP.parse("A \\/ ~ B /\\ C").toString == "(A \\/ (~B /\\ C))")
-    assert(PLP.parse("A /\\ B => C").toString == "((A /\\ B) => C)")
-    assert(PLP.parse("~A <=> (B /\\ C)").toString == "(~A <=> (B /\\ C))")
-    assert(PLP.parse("~A <=>   B /\\ C").toString == "(~A <=> (B /\\ C))")
-    assert(PLP.parse("~ A /\\ B => S /\\ T").toString == "((~A /\\ B) => (S /\\ T))")
-    assert(PLP.parse("~ A \\/ B => S /\\ T").toString == "((~A \\/ B) => (S /\\ T))")
-    assert(PLP.parse("~ A \\/ (B => C) /\\ D").toString == "(~A \\/ ((B => C) /\\ D))")
-    assert(PLP.parse("( norvig \\/ aima \\/ lisp ) /\\ (lisp => cool)").toString == 
-                    "((norvig \\/ aima \\/ lisp) /\\ (lisp => cool))")
+    expect(A)(PLP.parse("A"))
+    expect(PropositionSymbol("True"))(PLP.parse("True"))
+    expect(PropositionSymbol("False"))(PLP.parse("False"))
+    expect(new Negation(PropositionSymbol("A12")))(PLP.parse("~ A12"))
+    expect(new Disjunction(A,B))(PLP.parse("A|B"))
+    expect(new Conjunction(A,B,C))(PLP.parse("A & B & C"))
+    expect(new Disjunction(A,B,C))(PLP.parse("A | B | C"))
+    expect(new Disjunction(A,new Conjunction(B,C)))(PLP.parse("A | B & C"))
+    expect(new Disjunction(A,new Conjunction(new Negation(B),C)))(PLP.parse("A | ~ B & C"))
+    expect(new Conditional(new Conjunction(A,B),C))(PLP.parse("A & B => C"))
+    expect(new BiConditional(new Negation(A),new Conjunction(B,C)))(PLP.parse("~A <=> (B & C)"))
+    
+    expect(
+      new Conditional(
+        new Conjunction(new Negation(A),B),
+        new Conjunction(S,T))
+      )(PLP.parse("~ A & B => S & T"))
+    
+    expect(
+      new Conditional(
+        new Disjunction(new Negation(A),B),
+        new Conjunction(S,T))
+      )(PLP.parse("~ A | B => S & T"))
+    
+    expect(
+      new Disjunction(new Negation(A),
+                      new Conjunction(new Conditional(B,C),D))
+    )(PLP.parse("~ A | (B => C) & D"))
+    
+    expect(
+      new Conjunction(
+        new Disjunction(norvig,aima,lisp),
+        new Conditional(lisp,cool))
+    )(PLP.parse("( norvig | aima | lisp ) & (lisp => cool)"))
   }
 }
