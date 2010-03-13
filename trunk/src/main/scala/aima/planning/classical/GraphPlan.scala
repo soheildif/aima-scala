cbca6a0ce38b674549eea29e1bd9aff8d9a45ea2
g.himanshu <g.himanshu@91973ec2-87f0-11de-a4f2-b7219ab9ea91>
2010-03-13 06:55:13 +0000

    classical planning... wip

---
+++ trunk/src/main/scala/aima/planning/classical/GraphPlan.scala
@@ -0,0 +1,76 @@
+package aima.planning.classical
+
+/** GRAPHPLAN algorithm, described in Fig 10.9
+ *
+ * @author Himanshu Gupta
+ */
+object GraphPlan {
+  def apply(problem: ClassicalPlanningProblem): Option[Sol] = {
+    val graph = new PlanningGraph(problem.initState, problem.actions)
+    val goals = problem.goals
+    val noGoods = Map()
+    
+    def loop(tl: Int, graph: PlanningGraph): Option[Sol] = {
+      if goals.subsetOf(graph.stateLevel(tl).freeLiterals) {
+        val sol = extractSolution(graph,goals,nogoods)
+        if(sol != None) sol
+        
+      }
+
+      if (graph.isLeveledOff && nogoods.isLeveledOff) None
+      else loop(tl+1, expandGraph(graph))
+    }
+
+    loop(0,initPlanningGraph(problem))
+  }
+
+  def extractSolution(graph: PlanningGraph, goals: Set[Literal], n: Int, nogoods: Map) =
+    BacktrackingSearch(createCSP(graph)) match {
+      case None => None
+      case Some(m) => //convert to list of actions, return it
+  }
+
+  def createCSP(graph: PlanningGraph) = {
+    val domain = List(true,false)
+    val prob = new CSP[Action,Boolean]()
+
+    for(aLevel <- graph.actionLevels) {
+      for(action <- aLevel.actions)
+        prob.addVariables((action.toString,0), domain)
+      //add all the mutex constraints(mutex is a pair of actions)
+      prob.addConstraints(
+      aLevel.mutexes.map(_ match {
+        case (x,y) => new AllDiffConstraint(x.toString,y.toString)
+      }):_*)
+    }
+
+    prob
+  }
+}
+
+class PlanningGraph(initState: Set[Literal], actionSchema: Set[Action]) {
+
+  private currLevel = -1
+
+  var stateLevels: List[StateLevel]
+  var actionLevels: List[ActionLevel]
+
+  def init {
+    //create Level S0
+    val allPositives = collectPositiveLiterals(actionSchema)
+    val literals = initState ++ (allPositives - initState).map(_.makeNegative) :: statelevels
+    val mutexes = getMutexes(literals, prevLevel: Option[ActionLevel])
+    stateLevels = new StateLevel(literals,mutexes) :: stateLevels
+  }
+
+  def createNextLevel {
+    actionLevels = getNextActionLevel() :: actionLevels
+    stateLevels = getNextStateLevel
+  }
+
+  def getNextStateLevel(prevActionLevel: ActionLevel): StateLevel
+  def getNextActionLevel(prevStateLevel: StateLevel) : ActionLevel
+
+  def getLiteralMutexes(literals: Set[Literal], prevLevel: Option[ActionLevel]): Set[(Literal,Literal)]
+  def getActionMutexes(actions: Set[Action], prevLevel: StateLevel): Set[(Action,Action)]
+}
