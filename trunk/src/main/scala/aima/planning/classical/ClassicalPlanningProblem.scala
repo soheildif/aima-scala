efa2137fddfaffea1858aa0eee4e730da2666233
g.himanshu <g.himanshu@91973ec2-87f0-11de-a4f2-b7219ab9ea91>
2010-03-14 09:05:30 +0000

    classical planning ... wip

--- trunk/src/main/scala/aima/planning/classical/ClassicalPlanningProblem.scala
+++ trunk/src/main/scala/aima/planning/classical/ClassicalPlanningProblem.scala
@@ -5,12 +5,14 @@ package aima.planning.classical
  *
  * @author Himanshu Gupta
  */
-class ClassicalPlanningProblem(initState: Set[Literal], goals: Set[Literal], actions: Action *)
-
-
+class ClassicalPlanningProblem(val initState: Set[Literal], val goals: Set[Literal],
+                               val actions: Set[Action])
 object ClassicalPlanningProblem {
-
-  def apply(initState: String, goals: String, actions: Action *)
+  def apply(initState: String, goals: String, actions: Action *) =
+    new ClassicalPlanningProblem(
+      CPSentenceParser.parseSet(initState),
+      CPSentenceParser.parseSet(goals),
+      Set(actions:_*))
 }
 
 
@@ -26,11 +28,20 @@ class Action(val symbol: Atom, val preconditions: Set[Literal], val effects: Set
   
   override def toString = symbol.toString
 }
+object Action {
+  def apply(symbol: String, preconds: String, effects: String) =
+    new Action(
+      CPSentenceParser.parseAtom(symbol),
+      CPSentenceParser.parseSet(preconds),
+      CPSentenceParser.parseSet(effects))
+}
 
+//---------- AST for Classical Planning Variable Free Sentence ---------
+abstract class Sentence
 
 //Used to represent a predicate( e.g. Have(Cake))
 //as well as an action name(e.g. Eat(Cake))
-class Atom(val symbol: String, val args: List[String]) {
+class Atom(val symbol: String, val args: List[String]) extends Sentence {
 
   override def equals(that: Any) =
     that match {
@@ -46,6 +57,9 @@ class Atom(val symbol: String, val args: List[String]) {
     if(args.size == 0) "()" else "(" + args.reduceLeft(_ + "," + _) + ")"
   }
 }
+
+class Negation(val sentence: Atom) extends Sentence
+class Conjunction(val conjuncts: Set[Sentence]) extends Sentence
 
 //Positive and Negative Literals
 sealed class Literal(val sentence: Atom) {
@@ -63,3 +77,40 @@ sealed class Literal(val sentence: Atom) {
 }
 case class PositiveLiteral(s: Atom) extends Literal(s)
 case class NegativeLiteral(s: Atom) extends Literal(s)
+
+
+//---------------- Parser ---------------------
+object CPSentenceParser extends JavaTokenParsers {
+
+
+  def sentence: Parser[Sentence] = negation~rep("&"~negation) ^^
+                                      {case d~Nil => d
+                                       case d~reps => new Conjunction(d :: reps.map(_ match { case _~disj => disj }))}
+  
+
+  def negation: Parser[Sentence] = opt("~")~atom ^^
+                                    {case None~as => as
+                                     case Some(_)~as => new Negation(as)}
+
+  def atom: Parser[Atom] = (
+    constant~"("~repsep(constant,",")~")" ^^
+    {case s~"("~ts~")" => new Atom(s,ts)}
+  )
+                                         
+  def constant = """[A-Z][a-z0-9A-Z]*""".r
+
+  def parseSet(in: String): Set[Literal] =
+    parseAll(sentence,in).get match {
+      case x: Atom => Set(PositiveLiteral(x))
+      case x: Negation => Set(NegatveLiteral(x.sentence))
+      case x: Conjunction =>
+        x.conjuncts.map(
+          _ match {
+            case y: Atom => Set(PositiveLiteral(y))
+            case y: Negation => Set(NegativeLiteral(y.sentence))
+          })
+    }
+
+  def parseAtom(in: String): Atom =
+    parseAll(atom,in)
+}
