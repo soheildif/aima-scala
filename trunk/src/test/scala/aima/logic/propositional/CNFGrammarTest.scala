0bff6a9fb883bb5e4cbab4870383b283b3600e6d
g.himanshu <g.himanshu@91973ec2-87f0-11de-a4f2-b7219ab9ea91>
2010-04-02 08:35:49 +0000

    migrated propositional/CNFGrammarTest.scala to junit

--- trunk/src/test/scala/aima/logic/propositional/CNFGrammarTest.scala
+++ trunk/src/test/scala/aima/logic/propositional/CNFGrammarTest.scala
@@ -1,7 +1,7 @@
 package aima.logic.propositional
 
-import org.scalatest.Suite
-import scala.collection.immutable.{Set}
+import junit.framework._
+import Assert._
 
 /* This file contains tests for CNF sentence Grammar
  * described in Fig 7.14
@@ -9,7 +9,7 @@ import scala.collection.immutable.{Set}
  * @author Himanshu Gupta
  */
 
-class CNFGrammarTest extends Suite {
+class CNFGrammarTest extends TestCase {
   
   private val PLP = PropositionalLogicParser
 
@@ -27,48 +27,48 @@ class CNFGrammarTest extends Suite {
   private val cool = PropositionSymbol("cool")
 
   def testIt() {
-    expect(new CNFSentence(Set(new Clause(PositiveLiteral(A)))))(SentenceToCNF(PLP.parse("A")))
-    expect(new CNFSentence(Set(new Clause(NegativeLiteral(A)))))(SentenceToCNF(PLP.parse("~ A")))
+    assertEquals(new CNFSentence(Set(new Clause(PositiveLiteral(A)))),SentenceToCNF(PLP.parse("A")))
+    assertEquals(new CNFSentence(Set(new Clause(NegativeLiteral(A)))),SentenceToCNF(PLP.parse("~ A")))
 
-    expect(new CNFSentence(Set(new Clause(
+    assertEquals(new CNFSentence(Set(new Clause(
       PositiveLiteral(A),
-      PositiveLiteral(B)))))(SentenceToCNF(PLP.parse("A | B")))
+      PositiveLiteral(B)))),SentenceToCNF(PLP.parse("A | B")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(PositiveLiteral(A)),
       new Clause(PositiveLiteral(B)),
-      new Clause(PositiveLiteral(C))))
-         )(SentenceToCNF(PLP.parse("A & B & C")))
+      new Clause(PositiveLiteral(C)))),
+      SentenceToCNF(PLP.parse("A & B & C")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(
         PositiveLiteral(A),
         PositiveLiteral(B),
-        PositiveLiteral(C))))
-         )(SentenceToCNF(PLP.parse("A | B | C")))
+        PositiveLiteral(C)))),
+        SentenceToCNF(PLP.parse("A | B | C")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(PositiveLiteral(A),
                  PositiveLiteral(B)),
       new Clause(PositiveLiteral(A),
-                 PositiveLiteral(C))))
-         )(SentenceToCNF(PLP.parse("A | B & C")))
+                 PositiveLiteral(C)))),
+                 SentenceToCNF(PLP.parse("A | B & C")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(PositiveLiteral(A),
                  NegativeLiteral(B)),
       new Clause(PositiveLiteral(A),
-                 PositiveLiteral(C))))
-         )(SentenceToCNF(PLP.parse("A | ~ B & C")))
+                 PositiveLiteral(C)))),
+                 SentenceToCNF(PLP.parse("A | ~ B & C")))
     
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(
         NegativeLiteral(A),
         NegativeLiteral(B),
-        PositiveLiteral(C))))
-         )(SentenceToCNF(PLP.parse("A & B => C")))
+        PositiveLiteral(C)))),
+                 SentenceToCNF(PLP.parse("A & B => C")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(PositiveLiteral(A),
                  PositiveLiteral(B)),
       new Clause(PositiveLiteral(A),
@@ -76,10 +76,10 @@ class CNFGrammarTest extends Suite {
       new Clause(
         NegativeLiteral(A),
         NegativeLiteral(B),
-        NegativeLiteral(C))))
-           )(SentenceToCNF(PLP.parse("~A <=> (B & C)")))
+        NegativeLiteral(C)))),
+                 SentenceToCNF(PLP.parse("~A <=> (B & C)")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(
         PositiveLiteral(A),
         NegativeLiteral(B),
@@ -87,10 +87,10 @@ class CNFGrammarTest extends Suite {
       new Clause(
         PositiveLiteral(A),
         NegativeLiteral(B),
-        PositiveLiteral(T))))
-           )(SentenceToCNF(PLP.parse("~A & B => S & T")))
+        PositiveLiteral(T)))),
+                 SentenceToCNF(PLP.parse("~A & B => S & T")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(PositiveLiteral(A),
                  PositiveLiteral(S)),
       new Clause(PositiveLiteral(A),
@@ -98,27 +98,27 @@ class CNFGrammarTest extends Suite {
       new Clause(NegativeLiteral(B),
                  PositiveLiteral(S)),
       new Clause(NegativeLiteral(B),
-                 PositiveLiteral(T))))
-         )(SentenceToCNF(PLP.parse("~ A | B => S & T")))
+                 PositiveLiteral(T)))),
+                 SentenceToCNF(PLP.parse("~ A | B => S & T")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(
         NegativeLiteral(A),
         NegativeLiteral(B),
         PositiveLiteral(C)),
       new Clause(
         NegativeLiteral(A),
-        PositiveLiteral(D))))
-           )(SentenceToCNF(PLP.parse("~ A | (B => C) & D")))
+        PositiveLiteral(D)))),
+                 SentenceToCNF(PLP.parse("~ A | (B => C) & D")))
 
-    expect(new CNFSentence(Set(
+    assertEquals(new CNFSentence(Set(
       new Clause(
         PositiveLiteral(norvig),
         PositiveLiteral(aima),
         PositiveLiteral(lisp)),
       new Clause(
         NegativeLiteral(lisp),
-        PositiveLiteral(cool))))
-           )(SentenceToCNF(PLP.parse("( norvig | aima | lisp ) & (lisp => cool)")))
+        PositiveLiteral(cool)))),
+                 SentenceToCNF(PLP.parse("( norvig | aima | lisp ) & (lisp => cool)")))
   }
 }
